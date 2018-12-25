7e0357d5fb82c6b8754f0e194e7c81974bd93326
Filipe Paiva <filipe.p@soltroiavillas.com>
2018-12-25 18:54:47 +0000

    First commit + instructions

---
+++ lib/OTB/test.rb
@@ -0,0 +1,8 @@
+require "OTB/test/version"
+
+module OTB
+  module Test
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/OTB/test/version.rb
@@ -0,0 +1,5 @@
+module OTB
+  module Test
+    VERSION = "0.1.0"
+  end
+end
